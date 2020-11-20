90b953680a230b23ece754409bd92b0545e221e2
Ryan Tung <53037140+13126789556@users.noreply.github.com>
2020-11-20 14:50:17 -0800

    change to new branch

---
+++ Assets/Scripts/Parts/WoodTenon.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WoodTenon : MonoBehaviour
+{
+    //public string ID;
+    public Vector3 positionOffset;
+    public Vector3 angleOffset;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.matrix = transform.localToWorldMatrix;
+        Vector3 center = positionOffset;
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(center, Quaternion.Euler(angleOffset) * Vector3.right / 10 + center);
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(center, Quaternion.Euler(angleOffset) * Vector3.up / 10 + center);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(center, Quaternion.Euler(angleOffset) * Vector3.forward / 10 + center);
+    }
+}
