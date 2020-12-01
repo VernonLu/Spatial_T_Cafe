365e6d899f31131103c4b2ca0859d41831295b6f
VernonLu <55864722+VernonLu@users.noreply.github.com>
2020-12-01 13:37:15 -0800

    Fix C2 Remove M4-h

---
+++ Assets/Scripts/LevelSelection/CameraPath.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class CameraPath : MonoBehaviour
+{
+	protected LineRenderer path;
+
+	public Stage from;
+	public Stage dest;
+
+	void Start()
+	{
+		TryGetComponent(out path);
+		if (null == path)
+		{
+			Debug.Log(gameObject.name + " Don't have a Line Renderer component");
+			this.gameObject.SetActive(false);
+		}
+	}
+
+}
