c0d0453e153915085174dbd7f584d1ce69d0b74c
VernonLu <55864722+VernonLu@users.noreply.github.com>
2020-12-11 16:44:25 -0800

    Fix bugs

--- Assets/Scripts/Parts/WoodJoint.cs
+++ Assets/Scripts/Parts/WoodJoint.cs
@@ -1,6 +1,6 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
-using System;
 using UnityEngine;
 
 public class WoodJoint : MonoBehaviour
@@ -16,7 +16,7 @@ public class WoodJoint : MonoBehaviour
 
 	[Serializable]
 	public class ToleranceDomin
-    {
+	{
 		public Vector3 tolerancePositionStart;
 		public Vector3 tolerancePositionEnd;
 		//public Vector3 toleranceEularAngleStart;
@@ -43,38 +43,38 @@ public class WoodJoint : MonoBehaviour
 	private void Update()
 	{
 		//need optimize
-        if (!CheckStatus()) { return; }
-        Item currentItem = ControlManager.Instance.currentItem;
-        WoodTenon currentTenon = currentItem?.tenonList.Count > 0 ? currentItem.tenonList[0] : null;
+		if (!CheckStatus()) { return; }
+		Item currentItem = ControlManager.Instance.currentItem;
+		WoodTenon currentTenon = currentItem?.tenonList.Count > 0 ? currentItem.tenonList[0] : null;
 		//if(currentTenon == null)
-  //      {
+		//      {
 		//	return;
-  //      }
+		//      }
 		if (currentTenon != null && targetID == currentTenon.ID)
 		{
 			targetTenon = currentTenon;
 		}
-        if (IsTenonInCorrectPose())
-        {
-            // Debug.Log("Paired");
-            targetTenon.transform.parent.TryGetComponent(out Item item);
+		if (IsTenonInCorrectPose())
+		{
+			// Debug.Log("Paired");
+			targetTenon.transform.parent.TryGetComponent(out Item item);
 			if (item.isAssembled)
 			{
 				return;
 			}
-            item.isAssembled = true;
-            targetTenon.transform.parent.gameObject.SetActive(false);
+			item.isAssembled = true;
+			targetTenon.transform.parent.gameObject.SetActive(false);
 
-            // Move Camera to target position and rotation
-            MainCameraSwitch.Instance.SwitchOff();
-            TransitionCamera.Instance.SetTransform(cameraFocusPivot);
+			// Move Camera to target position and rotation
+			MainCameraSwitch.Instance.SwitchOff();
+			TransitionCamera.Instance.SetTransform(cameraFocusPivot);
 
-            // Enable sub part control
-            subPart.SetActive(true);
-            // Disable currentItem
-            ControlManager.Instance.currentItem?.gameObject.SetActive(false);
-        }
-    }
+			// Enable sub part control
+			subPart.SetActive(true);
+			// Disable currentItem
+			ControlManager.Instance.currentItem?.gameObject.SetActive(false);
+		}
+	}
 
 	/// <summary>
 	/// Check All dependenies are assembled
@@ -94,31 +94,30 @@ public class WoodJoint : MonoBehaviour
 	}
 
 	private bool IsTenonInCorrectPose()
-    {
-		if(targetTenon == null)
-        {
+	{
+		if (targetTenon == null)
+		{
 			return false;
-        }
+		}
 		//calculate if target in tolerance position
-		Vector3 startPos = Vector3.Min(toleranceDomin.tolerancePositionStart, toleranceDomin.tolerancePositionEnd) / 10;	//formlize
-		Vector3 endPos = Vector3.Max(toleranceDomin.tolerancePositionStart, toleranceDomin.tolerancePositionEnd) / 10;	//formlize
+		Vector3 startPos = Vector3.Min(toleranceDomin.tolerancePositionStart, toleranceDomin.tolerancePositionEnd) / 10; //formlize
+		Vector3 endPos = Vector3.Max(toleranceDomin.tolerancePositionStart, toleranceDomin.tolerancePositionEnd) / 10; //formlize
 		Vector3 deltaPos = targetTenon.transform.position - transform.position;
 		bool isPositionCorrect = Vector3.Min(startPos, deltaPos) == startPos && Vector3.Max(deltaPos, endPos) == endPos;
 		//calculate if target in tolerance angle
 		//Vector3 startAngle = Vector3.Min(toleranceDomin.toleranceEularAngleStart, toleranceDomin.toleranceEularAngleEnd);
 		Vector3 endAngle = Vector3.Max(-toleranceDomin.toleranceEularAngleEnd, toleranceDomin.toleranceEularAngleEnd);
 		Vector3 deltaAngle = targetTenon.transform.rotation.eulerAngles - transform.rotation.eulerAngles;
-        deltaAngle.x = Math.Abs(deltaAngle.x);
-        deltaAngle.y = Math.Abs(deltaAngle.y);
-        deltaAngle.z = Math.Abs(deltaAngle.z);
-        bool isAngleCorrect = /*Vector3.Min(startAngle, targetAngle) == startAngle && */Vector3.Max(deltaAngle, endAngle) == endAngle;
-        //return if target in correct pose
-        //print("pos: " + isPositionCorrect);
-  //      print("angle: " + isAngleCorrect + ", " + deltaAngle);
-		//print("targetAngle: " + targetTenon.transform.rotation.eulerAngles + "currentAngle: " + transform.rotation.eulerAngles);
-        return isPositionCorrect && isAngleCorrect;
-    }
-
+		deltaAngle.x = Math.Abs(deltaAngle.x);
+		deltaAngle.y = Math.Abs(deltaAngle.y);
+		deltaAngle.z = Math.Abs(deltaAngle.z);
+		bool isAngleCorrect = /*Vector3.Min(startAngle, targetAngle) == startAngle && */ Vector3.Max(deltaAngle, endAngle) == endAngle;
+		//return if target in correct pose
+		//print("pos: " + isPositionCorrect);
+		// print("angle: " + isAngleCorrect + ", " + deltaAngle);
+		// print("targetAngle: " + targetTenon.transform.rotation.eulerAngles + "currentAngle: " + transform.rotation.eulerAngles);
+		return isPositionCorrect && isAngleCorrect;
+	}
 
 	private void OnTriggerEnter(Collider other)
 	{
@@ -145,9 +144,8 @@ public class WoodJoint : MonoBehaviour
 
 	}
 
-
-    private void OnDrawGizmos()
-    {
+	private void OnDrawGizmos()
+	{
 		//Gizmos.color = Color.red;
 		//Gizmos.DrawLine(transform.position, transform.position + transform.right / 10);
 		//Gizmos.color = Color.green;
@@ -159,26 +157,26 @@ public class WoodJoint : MonoBehaviour
 		Gizmos.color = Color.green;
 		Vector3 center = (toleranceDomin.tolerancePositionStart + toleranceDomin.tolerancePositionEnd) / 2 / 10;
 		Gizmos.DrawWireCube(center, (toleranceDomin.tolerancePositionStart - toleranceDomin.tolerancePositionEnd) / 10);
-		if(toleranceDomin.toleranceEularAngleEnd.x != 0)
-        {
+		if (toleranceDomin.toleranceEularAngleEnd.x != 0)
+		{
 			Gizmos.color = Color.red;
 			Gizmos.DrawLine(Vector3.zero, Quaternion.Euler(-toleranceDomin.toleranceEularAngleEnd.x, 0, 0) * Vector3.up / 10);
 			Gizmos.color = Color.red;
 			Gizmos.DrawLine(Vector3.zero, Quaternion.Euler(toleranceDomin.toleranceEularAngleEnd.x, 0, 0) * Vector3.up / 10);
-        }
-		if(toleranceDomin.toleranceEularAngleEnd.y != 0)
-        {
+		}
+		if (toleranceDomin.toleranceEularAngleEnd.y != 0)
+		{
 			Gizmos.color = Color.green;
 			Gizmos.DrawLine(Vector3.zero, Quaternion.Euler(0, -toleranceDomin.toleranceEularAngleEnd.y, 0) * Vector3.forward / 10);
 			Gizmos.color = Color.green;
 			Gizmos.DrawLine(Vector3.zero, Quaternion.Euler(0, toleranceDomin.toleranceEularAngleEnd.y, 0) * Vector3.forward / 10);
-        }
-		if(toleranceDomin.toleranceEularAngleEnd.z != 0)
-        {
+		}
+		if (toleranceDomin.toleranceEularAngleEnd.z != 0)
+		{
 			Gizmos.color = Color.blue;
 			Gizmos.DrawLine(Vector3.zero, Quaternion.Euler(0, 0, -toleranceDomin.toleranceEularAngleEnd.z) * Vector3.right / 10);
 			Gizmos.color = Color.blue;
 			Gizmos.DrawLine(Vector3.zero, Quaternion.Euler(0, 0, toleranceDomin.toleranceEularAngleEnd.z) * Vector3.right / 10);
-        }
+		}
 	}
 }
