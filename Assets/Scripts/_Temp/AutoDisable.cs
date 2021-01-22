93ad9cbd259933c3fd6f3ca1dba7b4715aedb64c
VernonLu <55864722+VernonLu@users.noreply.github.com>
2021-01-22 15:16:19 -0800

    Update 2-3

---
+++ Assets/Scripts/_Temp/AutoDisable.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoDisable : MonoBehaviour
+{
+	public float waitTime = 0.5f;
+	private void OnEnable()
+	{
+		StartCoroutine(DisableSelf());
+	}
+
+	private IEnumerator DisableSelf()
+	{
+		yield return new WaitForSeconds(waitTime);
+		this.gameObject.SetActive(false);
+	}
+}
