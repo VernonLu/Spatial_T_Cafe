8a03fbff08f285f7f2d54a3d8b5a39c9b42f16c3
VernonLu <55864722+VernonLu@users.noreply.github.com>
2021-02-12 03:14:33 -0800

    Update 3-3 and Audio

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -11,13 +11,29 @@ public class AudioManager : MonoBehaviour
   private AudioEmitter levelBGM;
 
   private FMOD.Studio.Bus masterBus;
+  private FMOD.Studio.Bus bgmBus;
+  private FMOD.Studio.Bus sfxBus;
+
+  private float masterVolume;
+  private float bgmVolume = 0.5f;
+  private float sfxVolume = 0.2f;
 
   void Start()
   {
     masterBus = FMODUnity.RuntimeManager.GetBus("bus:/");
-		dataSounds.InitLoopSounds();
+    bgmBus = FMODUnity.RuntimeManager.GetBus("bus:/BGM");
+    sfxBus = FMODUnity.RuntimeManager.GetBus("bus:/SFX");
+    dataSounds.InitLoopSounds();
 
-    if(levelBGM) PlaySound(levelBGM);
+    if (levelBGM) PlaySound(levelBGM);
+  }
+
+  void Update()
+  {
+    var bgmResult = bgmBus.setVolume(bgmVolume);
+    // Debug.Log("BGM result: " + bgmResult);
+    var sfxResult = sfxBus.setVolume(sfxVolume);
+    // Debug.Log("SFX result: " + sfxResult);
   }
 
   public void PlaySound(AudioEmitter emitter)
@@ -57,6 +73,7 @@ public class AudioManager : MonoBehaviour
       sound.Set3DAttributes(emitter.Position);
     }
     sound.Start();
+    Debug.Log("Play Sound");
   }
 
   public void StopLoop(AudioEmitter emitter)
@@ -80,7 +97,7 @@ public class AudioManager : MonoBehaviour
     sound.Start();
   }
 
-  private void StopMainLoops()
+  public void StopMainLoops()
   {
     foreach (Sound loopSound in dataSounds.LoopSounds)
     {
@@ -107,5 +124,20 @@ public class AudioManager : MonoBehaviour
   public void SetMasterVolume(float volume)
   {
     masterBus.setVolume(volume);
+  }
+
+  public void SetBGMVolume(float volume)
+  {
+    float val = volume / 6;
+    var result = FMODUnity.RuntimeManager.GetBus("bus:/").setVolume(val);
+    Debug.Log("Set BGM Volume: " + result);
+    FMODUnity.RuntimeManager.GetBus("bus:/").getVolume(out float bgmVol, out float finalVol);
+    Debug.Log(bgmVol + " " + finalVol);
+  }
+
+  public void SetSFXVolume(float volume)
+  {
+    float val = (9999f / 60000f) * volume + 0.0001f;
+    sfxBus.setVolume(Mathf.Log10(val) * 20);
   }
 }
